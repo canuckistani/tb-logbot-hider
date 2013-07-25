d6546f7b7528b728d95c6b3bae9d8fa4767e6f3b
canuckistani <jeff@canuckistani.ca>
2013-07-24 23:16:49 -0700

    hide comments by default

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Logbot Hider
 
+Are you looking at bugs with lots of Tinderbox Pushlogbot comments? This add-on hides them by default, and also adds a link you can click if you actually want to see them.
--- data/cs.js
+++ data/cs.js
@@ -1,34 +1,32 @@
+var pushbot_c = 0;
+
 function setup() {
-  console.log('in setup');
-  var pushbot_c = 0;
   jQuery('div.bz_comment').each(function(i) {
     pushbot_c++;
   });
   jQuery('ul.bz_collapse_expand_comments')
-    .append('<li id="tb-log-toggle-action"><a href="#">Hide '+pushbot_c+' Pushlog Bot Comments</a></li>');
+    .append('<li id="tb-log-toggle-action"><a href="#">Show '+pushbot_c+' Pushlog Bot Comments</a></li>');
 
-  jQuery('#tb-log-toggle-action a').click(
-    function() {
-      var botlogs;
-      jQuery('div.bz_comment').each(function(i) {
-        if (jQuery(this).text().indexOf('TinderboxPushlog Robot') !== -1) {
-          // botlogs++;
-          jQuery(this).toggleClass('collapsed');
-          if (jQuery(this).hasClass('collapsed')) {
-            jQuery('#tb-log-toggle-action a').text('Show '+pushbot_c+' Pushlog Bot Comments');
-          }
-          else {
-            jQuery('#tb-log-toggle-action a').text('Hide '+pushbot_c+' Pushlog Bot Comments'); 
-          }
+  jQuery('#tb-log-toggle-action a').click(toggleComments);
+  toggleComments();
+}
 
-        }
-      });
-      return false;
+function toggleComments() {
+  var botlogs;
+  jQuery('div.bz_comment').each(function(i) {
+    if (jQuery(this).text().indexOf('TinderboxPushlog Robot') !== -1) {
+      jQuery(this).toggleClass('collapsed');
+      if (jQuery(this).hasClass('collapsed')) {
+        jQuery('#tb-log-toggle-action a').text('Show '+pushbot_c+' Pushlog Bot Comments');
+      }
+      else {
+        jQuery('#tb-log-toggle-action a').text('Hide '+pushbot_c+' Pushlog Bot Comments'); 
+      }
     }
-  );
+  });
+  return false;
 }
 
 self.port.on('attached', function() {
   setup();
 });
-
